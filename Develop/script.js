da43063205aa87be21761e1c7fa7518de22b61f9
DTSmith17 <153142161+DTSmith17@users.noreply.github.com>
2024-02-12 21:50:20 -0600

    commit test

--- Develop/script.js
+++ Develop/script.js
@@ -93,6 +93,8 @@ async function displayRecipes(recipes) {
   }
 }
 
+//changesto be commited
+
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
